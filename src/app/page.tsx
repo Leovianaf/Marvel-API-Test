a1194630986b5fbbfff18f5f204bcdeea8c4b7d7
Leonardo Viana <leonardoviana.nave@gmail.com>
2024-02-21 09:27:20 -0300

    Adição do EsLint e Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-"use client"
-import React, { use, useEffect, useState} from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/page.module.css";
 import api from "../lib/api";
-
-const baseURL = 'https://gateway.marvel.com/v1/public/characters';
 
 interface ResponseData {
   id: string;
@@ -19,31 +17,33 @@ const App: React.FC = () => {
   const [characters, setCharacters] = useState<ResponseData[]>([]);
   useEffect(() => {
     api
-      .get('/characters')
-      .then(response => {
-      const fetchedCharacters: ResponseData[] = response.data.data.results;
-      setCharacters(fetchedCharacters);
-      console.log("Characters: ", fetchedCharacters); 
+      .get("/characters")
+      .then((response) => {
+        const fetchedCharacters: ResponseData[] = response.data.data.results;
+        setCharacters(fetchedCharacters);
+        console.log("Characters: ", fetchedCharacters);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-
   return (
-      <div>
-        <h1>Characters</h1>
-        {characters.map((character) => {
-          return (
-            <div key={character.id} className={styles.character}>
-              <h2>{character.name}</h2>
-              <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt={character.name} />
-              <p>{character.description}</p>
-            </div>
-          );
-        })}
-      </div>
+    <div>
+      <h1>Characters</h1>
+      {characters.map((character) => {
+        return (
+          <div key={character.id} className={styles.character}>
+            <h2>{character.name}</h2>
+            <img
+              src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+              alt={character.name}
+            />
+            <p>{character.description}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
@@ -51,4 +51,3 @@ export default App;
 function useClient() {
   throw new Error("Function not implemented.");
 }
-
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import md5 from 'md5';
+import axios from "axios";
+import md5 from "md5";
 
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
@@ -8,20 +8,18 @@ const ts = new Date().getTime();
 let hash;
 
 if (!publicKey || !privateKey) {
-    console.error('Chaves não definidas.');
-} else { 
-    hash = md5(ts + privateKey + publicKey);
-} 
-
-
+  console.error("Chaves não definidas.");
+} else {
+  hash = md5(ts + privateKey + publicKey);
+}
 
 const api = axios.create({
-    baseURL: 'https://gateway.marvel.com/v1/public/',
-    params: {
-        ts,
-        apikey: publicKey,
-        hash,
-    },
+  baseURL: "https://gateway.marvel.com/v1/public/",
+  params: {
+    ts,
+    apikey: publicKey,
+    hash,
+  },
 });
 
 export default api;
